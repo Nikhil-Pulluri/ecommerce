df38302b7b7d0a94e86fb9a91233fbc444bdb37b
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2025-02-07 11:45:10 +0530

    jwt initialised and login route secured

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, HttpCode, Post, HttpStatus, Body } from '@nestjs/common';
+import { Controller, HttpCode, Post, HttpStatus, Body, UseGuards, Get, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.gaurd';
 
 @Controller('auth')
 export class AuthController {
@@ -11,5 +12,12 @@ export class AuthController {
     @Body() signInDto: Record<string, any>
   ){
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req)
+  {
+    return req.user
   }
 }
